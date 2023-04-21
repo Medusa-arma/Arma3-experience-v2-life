8cbcb0d89ce3710fda5b716977eb0e8dcd435067
System32 <89716900+System32-0@users.noreply.github.com>
2023-04-22 05:11:29 +1000

    Add 200 levels

--- exp/exp/Missionfolder/config/Config_Experience.hpp
+++ exp/exp/Missionfolder/config/Config_Experience.hpp
@@ -129,4 +129,724 @@ class Level_exp {
 		points = 1;
 		exp_req = 2000;
 	};
+	class 21_exp { 
+		points = 1;
+		exp_req = 2100;
+	};
+	class 22_exp { 
+		points = 1;
+		exp_req = 2200;
+	};
+	class 23_exp { 
+		points = 1;
+		exp_req = 2300;
+	};
+	class 24_exp { 
+		points = 1;
+		exp_req = 2400;
+	};
+	class 25_exp { 
+		points = 1;
+		exp_req = 2500;
+	};
+	class 26_exp { 
+		points = 1;
+		exp_req = 2600;
+	};
+	class 27_exp { 
+		points = 1;
+		exp_req = 2700;
+	};
+	class 28_exp { 
+		points = 1;
+		exp_req = 2800;
+	};
+	class 29_exp { 
+		points = 1;
+		exp_req = 2900;
+	};
+	class 30_exp { 
+		points = 1;
+		exp_req = 3000;
+	};
+	class 31_exp { 
+		points = 1;
+		exp_req = 3100;
+	};
+	class 32_exp { 
+		points = 1;
+		exp_req = 3200;
+	};
+	class 33_exp { 
+		points = 1;
+		exp_req = 3300;
+	};
+	class 34_exp { 
+		points = 1;
+		exp_req = 3400;
+	};
+	class 35_exp { 
+		points = 1;
+		exp_req = 3500;
+	};
+	class 36_exp { 
+		points = 1;
+		exp_req = 3600;
+	};
+	class 37_exp { 
+		points = 1;
+		exp_req = 3700;
+	};
+	class 38_exp { 
+		points = 1;
+		exp_req = 3800;
+	};
+	class 39_exp { 
+		points = 1;
+		exp_req = 3900;
+	};
+	class 40_exp { 
+		points = 1;
+		exp_req = 4000;
+	};
+	class 41_exp { 
+		points = 1;
+		exp_req = 4100;
+	};
+	class 42_exp { 
+		points = 1;
+		exp_req = 4200;
+	};
+	class 43_exp { 
+		points = 1;
+		exp_req = 4300;
+	};
+	class 44_exp { 
+		points = 1;
+		exp_req = 4400;
+	};
+	class 45_exp { 
+		points = 1;
+		exp_req = 4500;
+	};
+	class 46_exp { 
+		points = 1;
+		exp_req = 4600;
+	};
+	class 47_exp { 
+		points = 1;
+		exp_req = 4700;
+	};
+	class 48_exp { 
+		points = 1;
+		exp_req = 4800;
+	};
+	class 49_exp { 
+		points = 1;
+		exp_req = 4900;
+	};
+	class 50_exp { 
+		points = 1;
+		exp_req = 5000;
+	};
+	class 51_exp { 
+		points = 1;
+		exp_req = 5100;
+	};
+	class 52_exp { 
+		points = 1;
+		exp_req = 5200;
+	};
+	class 53_exp { 
+		points = 1;
+		exp_req = 5300;
+	};
+	class 54_exp { 
+		points = 1;
+		exp_req = 5400;
+	};
+	class 55_exp { 
+		points = 1;
+		exp_req = 5500;
+	};
+	class 56_exp { 
+		points = 1;
+		exp_req = 5600;
+	};
+	class 57_exp { 
+		points = 1;
+		exp_req = 5700;
+	};
+	class 58_exp { 
+		points = 1;
+		exp_req = 5800;
+	};
+	class 59_exp { 
+		points = 1;
+		exp_req = 5900;
+	};
+	class 60_exp { 
+		points = 1;
+		exp_req = 6000;
+	};
+	class 61_exp { 
+		points = 1;
+		exp_req = 6100;
+	};
+	class 62_exp { 
+		points = 1;
+		exp_req = 6200;
+	};
+	class 63_exp { 
+		points = 1;
+		exp_req = 6300;
+	};
+	class 64_exp { 
+		points = 1;
+		exp_req = 6400;
+	};
+	class 65_exp { 
+		points = 1;
+		exp_req = 6500;
+	};
+	class 66_exp { 
+		points = 1;
+		exp_req = 6600;
+	};
+	class 67_exp { 
+		points = 1;
+		exp_req = 6700;
+	};
+	class 68_exp { 
+		points = 1;
+		exp_req = 6800;
+	};
+	class 69_exp { 
+		points = 1;
+		exp_req = 6900;
+	};
+	class 70_exp { 
+		points = 1;
+		exp_req = 7000;
+	};
+	class 71_exp { 
+		points = 1;
+		exp_req = 7100;
+	};
+	class 72_exp { 
+		points = 1;
+		exp_req = 7200;
+	};
+	class 73_exp { 
+		points = 1;
+		exp_req = 7300;
+	};
+	class 74_exp { 
+		points = 1;
+		exp_req = 7400;
+	};
+	class 75_exp { 
+		points = 1;
+		exp_req = 7500;
+	};
+	class 76_exp { 
+		points = 1;
+		exp_req = 7600;
+	};
+	class 77_exp { 
+		points = 1;
+		exp_req = 7700;
+	};
+	class 78_exp { 
+		points = 1;
+		exp_req = 7800;
+	};
+	class 79_exp { 
+		points = 1;
+		exp_req = 7900;
+	};
+	class 80_exp { 
+		points = 1;
+		exp_req = 8000;
+	};
+	class 81_exp { 
+		points = 1;
+		exp_req = 8100;
+	};
+	class 82_exp { 
+		points = 1;
+		exp_req = 8200;
+	};
+	class 83_exp { 
+		points = 1;
+		exp_req = 8300;
+	};
+	class 84_exp { 
+		points = 1;
+		exp_req = 8400;
+	};
+	class 85_exp { 
+		points = 1;
+		exp_req = 8500;
+	};
+	class 86_exp { 
+		points = 1;
+		exp_req = 8600;
+	};
+	class 87_exp { 
+		points = 1;
+		exp_req = 8700;
+	};
+	class 88_exp { 
+		points = 1;
+		exp_req = 8800;
+	};
+	class 89_exp { 
+		points = 1;
+		exp_req = 8900;
+	};
+	class 90_exp { 
+		points = 1;
+		exp_req = 9000;
+	};
+	class 91_exp { 
+		points = 1;
+		exp_req = 9100;
+	};
+	class 92_exp { 
+		points = 1;
+		exp_req = 9200;
+	};
+	class 93_exp { 
+		points = 1;
+		exp_req = 9300;
+	};
+	class 94_exp { 
+		points = 1;
+		exp_req = 9400;
+	};
+	class 95_exp { 
+		points = 1;
+		exp_req = 9500;
+	};
+	class 96_exp { 
+		points = 1;
+		exp_req = 9600;
+	};
+	class 97_exp { 
+		points = 1;
+		exp_req = 9700;
+	};
+	class 98_exp { 
+		points = 1;
+		exp_req = 9800;
+	};
+	class 99_exp { 
+		points = 1;
+		exp_req = 9900;
+	};
+	class 100_exp { 
+		points = 1;
+		exp_req = 10000;
+	};
+	class 101_exp { 
+		points = 1;
+		exp_req = 10100;
+	};
+	class 102_exp { 
+		points = 1;
+		exp_req = 10200;
+	};
+	class 103_exp { 
+		points = 1;
+		exp_req = 10300;
+	};
+	class 104_exp { 
+		points = 1;
+		exp_req = 10400;
+	};
+	class 105_exp { 
+		points = 1;
+		exp_req = 10500;
+	};
+	class 106_exp { 
+		points = 1;
+		exp_req = 10600;
+	};
+	class 107_exp { 
+		points = 1;
+		exp_req = 10700;
+	};
+	class 108_exp { 
+		points = 1;
+		exp_req = 10800;
+	};
+	class 109_exp { 
+		points = 1;
+		exp_req = 10900;
+	};
+	class 110_exp { 
+		points = 1;
+		exp_req = 11000;
+	};
+	class 111_exp { 
+		points = 1;
+		exp_req = 11100;
+	};
+	class 112_exp { 
+		points = 1;
+		exp_req = 11200;
+	};
+	class 113_exp { 
+		points = 1;
+		exp_req = 11300;
+	};
+	class 114_exp { 
+		points = 1;
+		exp_req = 11400;
+	};
+	class 115_exp { 
+		points = 1;
+		exp_req = 11500;
+	};
+	class 116_exp { 
+		points = 1;
+		exp_req = 11600;
+	};
+	class 117_exp { 
+		points = 1;
+		exp_req = 11700;
+	};
+	class 118_exp { 
+		points = 1;
+		exp_req = 11800;
+	};
+	class 119_exp { 
+		points = 1;
+		exp_req = 11900;
+	};
+	class 120_exp { 
+		points = 1;
+		exp_req = 12000;
+	};
+	class 121_exp { 
+		points = 1;
+		exp_req = 12100;
+	};
+	class 122_exp { 
+		points = 1;
+		exp_req = 12200;
+	};
+	class 123_exp { 
+		points = 1;
+		exp_req = 12300;
+	};
+	class 124_exp { 
+		points = 1;
+		exp_req = 12400;
+	};
+	class 125_exp { 
+		points = 1;
+		exp_req = 12500;
+	};
+	class 126_exp { 
+		points = 1;
+		exp_req = 12600;
+	};
+	class 127_exp { 
+		points = 1;
+		exp_req = 12700;
+	};
+	class 128_exp { 
+		points = 1;
+		exp_req = 12800;
+	};
+	class 129_exp { 
+		points = 1;
+		exp_req = 12900;
+	};
+	class 130_exp { 
+		points = 1;
+		exp_req = 13000;
+	};
+	class 131_exp { 
+		points = 1;
+		exp_req = 13100;
+	};
+	class 132_exp { 
+		points = 1;
+		exp_req = 13200;
+	};
+	class 133_exp { 
+		points = 1;
+		exp_req = 13300;
+	};
+	class 134_exp { 
+		points = 1;
+		exp_req = 13400;
+	};
+	class 135_exp { 
+		points = 1;
+		exp_req = 13500;
+	};
+	class 136_exp { 
+		points = 1;
+		exp_req = 13600;
+	};
+	class 137_exp { 
+		points = 1;
+		exp_req = 13700;
+	};
+	class 138_exp { 
+		points = 1;
+		exp_req = 13800;
+	};
+	class 139_exp { 
+		points = 1;
+		exp_req = 13900;
+	};
+	class 140_exp { 
+		points = 1;
+		exp_req = 14000;
+	};
+	class 141_exp { 
+		points = 1;
+		exp_req = 14100;
+	};
+	class 142_exp { 
+		points = 1;
+		exp_req = 14200;
+	};
+	class 143_exp { 
+		points = 1;
+		exp_req = 14300;
+	};
+	class 144_exp { 
+		points = 1;
+		exp_req = 14400;
+	};
+	class 145_exp { 
+		points = 1;
+		exp_req = 14500;
+	};
+	class 146_exp { 
+		points = 1;
+		exp_req = 14600;
+	};
+	class 147_exp { 
+		points = 1;
+		exp_req = 14700;
+	};
+	class 148_exp { 
+		points = 1;
+		exp_req = 14800;
+	};
+	class 149_exp { 
+		points = 1;
+		exp_req = 14900;
+	};
+	class 150_exp { 
+		points = 1;
+		exp_req = 15000;
+	};
+	class 151_exp { 
+		points = 1;
+		exp_req = 15100;
+	};
+	class 152_exp { 
+		points = 1;
+		exp_req = 15200;
+	};
+	class 153_exp { 
+		points = 1;
+		exp_req = 15300;
+	};
+	class 154_exp { 
+		points = 1;
+		exp_req = 15400;
+	};
+	class 155_exp { 
+		points = 1;
+		exp_req = 15500;
+	};
+	class 156_exp { 
+		points = 1;
+		exp_req = 15600;
+	};
+	class 157_exp { 
+		points = 1;
+		exp_req = 15700;
+	};
+	class 158_exp { 
+		points = 1;
+		exp_req = 15800;
+	};
+	class 159_exp { 
+		points = 1;
+		exp_req = 15900;
+	};
+	class 160_exp { 
+		points = 1;
+		exp_req = 16000;
+	};
+	class 161_exp { 
+		points = 1;
+		exp_req = 16100;
+	};
+	class 162_exp { 
+		points = 1;
+		exp_req = 16200;
+	};
+	class 163_exp { 
+		points = 1;
+		exp_req = 16300;
+	};
+	class 164_exp { 
+		points = 1;
+		exp_req = 16400;
+	};
+	class 165_exp { 
+		points = 1;
+		exp_req = 16500;
+	};
+	class 166_exp { 
+		points = 1;
+		exp_req = 16600;
+	};
+	class 167_exp { 
+		points = 1;
+		exp_req = 16700;
+	};
+	class 168_exp { 
+		points = 1;
+		exp_req = 16800;
+	};
+	class 169_exp { 
+		points = 1;
+		exp_req = 16900;
+	};
+	class 170_exp { 
+		points = 1;
+		exp_req = 17000;
+	};
+	class 171_exp { 
+		points = 1;
+		exp_req = 17100;
+	};
+	class 172_exp { 
+		points = 1;
+		exp_req = 17200;
+	};
+	class 173_exp { 
+		points = 1;
+		exp_req = 17300;
+	};
+	class 174_exp { 
+		points = 1;
+		exp_req = 17400;
+	};
+	class 175_exp { 
+		points = 1;
+		exp_req = 17500;
+	};
+	class 176_exp { 
+		points = 1;
+		exp_req = 17600;
+	};
+	class 177_exp { 
+		points = 1;
+		exp_req = 17700;
+	};
+	class 178_exp { 
+		points = 1;
+		exp_req = 17800;
+	};
+	class 179_exp { 
+		points = 1;
+		exp_req = 17900;
+	};
+	class 180_exp { 
+		points = 1;
+		exp_req = 18000;
+	};
+	class 181_exp { 
+		points = 1;
+		exp_req = 18100;
+	};
+	class 182_exp { 
+		points = 1;
+		exp_req = 18200;
+	};
+	class 183_exp { 
+		points = 1;
+		exp_req = 18300;
+	};
+	class 184_exp { 
+		points = 1;
+		exp_req = 18400;
+	};
+	class 185_exp { 
+		points = 1;
+		exp_req = 18500;
+	};
+	class 186_exp { 
+		points = 1;
+		exp_req = 18600;
+	};
+	class 187_exp { 
+		points = 1;
+		exp_req = 18700;
+	};
+	class 188_exp { 
+		points = 1;
+		exp_req = 18800;
+	};
+	class 189_exp { 
+		points = 1;
+		exp_req = 18900;
+	};
+	class 190_exp { 
+		points = 1;
+		exp_req = 19000;
+	};
+	class 191_exp { 
+		points = 1;
+		exp_req = 19100;
+	};
+	class 192_exp { 
+		points = 1;
+		exp_req = 19200;
+	};
+	class 193_exp { 
+		points = 1;
+		exp_req = 19300;
+	};
+	class 194_exp { 
+		points = 1;
+		exp_req = 19400;
+	};
+	class 195_exp { 
+		points = 1;
+		exp_req = 19500;
+	};
+	class 196_exp { 
+		points = 1;
+		exp_req = 19600;
+	};
+	class 197_exp { 
+		points = 1;
+		exp_req = 19700;
+	};
+	class 198_exp { 
+		points = 1;
+		exp_req = 19800;
+	};
+	class 199_exp { 
+		points = 1;
+		exp_req = 19900;
+	};
+	class 200_exp { 
+		points = 1;
+		exp_req = 20000;
+	};
 };
